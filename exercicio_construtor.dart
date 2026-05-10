8b1d3254e11e008252070713df4706528a204e96
Rafael Aguiar <rafael.henrique.aguiar@outlook.com.br>
2026-05-10 11:42:39 -0300

    adicionado exercicio

---
+++ exercicio_construtor.dart
@@ -0,0 +1,8 @@
+void main() {}
+
+List<Map<String, dynamic>> alunos = [
+  {"id": 98523488, "nome": "João da Silva", "telefone": null},
+  {"id": 98523489, "nome": "Lucas Evangelista", "telefone": '11996541236'},
+  {"id": 98523490, "nome": "Maria Teodoro", "telefone": '11944551236'},
+  {"id": 98523491, "nome": "Isabel Santos", "telefone": null},
+];
